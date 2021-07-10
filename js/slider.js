786f751cff5a6dfed2213b86f13c7491a7492bc6
mistexx <82925870+mistexx@users.noreply.github.com>
2021-07-10 19:54:34 +0300

    Add files via upload

--- js/slider.js
+++ js/slider.js
@@ -3,40 +3,61 @@ const buttonRight = document.querySelector("#sliderRight");
 const itemsList = document.querySelector("#slider");
 const items = document.querySelectorAll(".assortment__item");
 
-const step = items[0].getBoundingClientRect().width;
+let step = items[0].getBoundingClientRect().width;
 const minRight = 0;
-const maxRight = (items.length - 1) * step;
+let maxRight = (items.length - 1) * step;
 
 let currentRight = 0;
+let currentStep = 0;
+
+const listStyles = getComputedStyle(itemsList);
+const initialTransition = `${listStyles["transition-property"]} ${listStyles["transition-duration"]}`;
+
 
 itemsList.style.right = currentRight;
 
+function updateSlider(params) {
+    itemsList.style.transition = 'none';
+    step = items[0].getBoundingClientRect().width;
+    maxRight = (items.length - 1) * step;
+    currentRight = step * currentStep;
+    itemsList.style.right = `${currentRight}px`;
+}
+
+window.addEventListener("resize", updateSlider);
+
 buttonRight.addEventListener("click", e => {
     e.preventDefault();
+    itemsList.style.transition = initialTransition;
 
     if (currentRight === maxRight) {
         currentRight = minRight;
         itemsList.style.right = `${currentRight}px`;
+        currentStep = 0;
         return;
     }
 
     if (currentRight < maxRight) {
         currentRight += step;
         itemsList.style.right = `${currentRight}px`;
+        currentStep++;
     }
 });
 
 buttonLeft.addEventListener("click", e => {
     e.preventDefault();
+    itemsList.style.transition = initialTransition;
 
     if (currentRight === minRight) {
         currentRight = maxRight;
         itemsList.style.right = `${currentRight}px`;
+        currentStep = items.length - 1;
         return;
     }
 
     if (currentRight > minRight) {
         currentRight -= step;
         itemsList.style.right = `${currentRight}px`;
+        currentStep--;
     }
 });
