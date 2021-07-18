2d2261736983cdb459b86a54b6435f132f9377a1
mistexx <82925870+mistexx@users.noreply.github.com>
2021-07-18 22:31:08 +0300

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,12 @@ $(".form").submit(e => {
     const comment = form.find("[name='comment']");
     const to = form.find("[name='to']");
     const isValid = validateForm(form[0]);
+    
+    const modal = $("#modal");
+    const content = modal.find(".modal__title");
+
+    modal.removeClass("error-modal"); 
+
     if (isValid) {
        $.ajax({
            url: "https://webdev-api.loftschool.com/sendmail",
@@ -21,8 +27,18 @@ $(".form").submit(e => {
                to: to.val(),
            },
            success: data => {
-               console.log(data);
+            content.text(data.message)
             $.fancybox.open({
+                src: "#modal",
+                type: "inline"
+            })
+           },
+           error: data => {
+               const message = data.responseJSON.message;
+               content.text(message);
+               console.log(data);
+               modal.addClass("error-modal"); 
+               $.fancybox.open({
                 src: "#modal",
                 type: "inline"
             })
